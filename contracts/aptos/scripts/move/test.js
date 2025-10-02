0bee946492dbcc5668b5285d58958b6b394b8aac
Xxiled <93330009+xxiled-plastic-cat@users.noreply.github.com>
2025-10-02 21:52:06 +0100

    build info

--- contracts/aptos/scripts/move/test.js
+++ contracts/aptos/scripts/move/test.js
@@ -8,7 +8,7 @@ async function test() {
   await move.test({
     packageDirectoryPath: "contract",
     namedAddresses: {
-      waypoint: "0x42",
+      waypoint: "0x12dd47c0156dc2237a6e814b227bb664f54e85332ff636a64bc9dd1ce7d1bdb0",
     },
   });
 }
